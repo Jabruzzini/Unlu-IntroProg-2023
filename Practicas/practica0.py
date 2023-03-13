b4cd7bb7734d3c286e62d6c2e06f47dee65e54df
Francisco Joaquin Abruzzini <101670872+Jabruzzini@users.noreply.github.com>
2023-03-12 21:38:02 -0300

    Practicas primer dia

---
+++ Practicas/practica0.py
@@ -0,0 +1,21 @@
+# n1=(int(input("Escriba un  numero : ")))
+# n2=(int(input("Escriba un segundo numero : ")))
+
+# res=n1+n2
+
+# print ("El resultado de sumar",n1,"+",n2,"  dio como resultado",res)
+
+# ----------------------------------------------------------------------
+
+# Variables que almacenan datos
+
+
+x=47
+y=68
+
+# Variable que almacena la operacion entre las dos variables anteriores
+z=x+y
+
+# Lo que se imprime en pantalla y ve el usuario
+
+print ("El resultado de sumar",x,"+",y,"  dio como resultado",z)
