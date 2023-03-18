c432e55489c1bd1ebddf627cd7b125a4450628c8
Francisco Joaquin Abruzzini <101670872+Jabruzzini@users.noreply.github.com>
2023-03-18 18:47:58 -0300

    Practicando funciones

--- Practicas/practica0.py
+++ Practicas/practica0.py
@@ -10,12 +10,34 @@
 # Variables que almacenan datos
 
 
-x=47
-y=68
+# x = 47
+# y = 68
 
 # Variable que almacena la operacion entre las dos variables anteriores
-z=x+y
+# z = x+y
 
 # Lo que se imprime en pantalla y ve el usuario
 
-print ("El resultado de sumar",x,"+",y,"  dio como resultado",z)
+# print ("El resultado de sumar",x,"+",y,"  dio como resultado",z)
+
+
+# """ 
+#     FUNCIONES >>>  es importante recordar que : 
+
+#     1 ) Se debe documentar una funcion detallando 
+#         parametros de entrada y resultado de salida ( que devuelve )
+#     2) Hacerlo de este forma usando triple comilla
+#      3) es mas reutilizable si las funciones reciben los datos como parametros y no los escribimos dentro de la funcion 
+# """
+
+
+# def multi_parametros(x, y):
+#     """Esta funcion recibe dos parametros y como resultado da su multiplicacion"""
+#     multiplicacion = x*y
+#     return multiplicacion
+
+
+# x = multi_parametros(10, 2)
+
+# print("Multiplicacion de numeros: ", x)
+
