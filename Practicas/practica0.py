6b01575722a3049039d53b40ce5ee766cdd557f4
Francisco Joaquin Abruzzini <101670872+Jabruzzini@users.noreply.github.com>
2023-03-18 19:50:32 -0300

    cambios

--- Practicas/practica0.py
+++ Practicas/practica0.py
@@ -1,5 +1,5 @@
-# n1=(int(input("Escriba un  numero : ")))
-# n2=(int(input("Escriba un segundo numero : ")))
+n1=(int(input("Escriba un  numero : ")))
+n2=(int(input("Escriba un segundo numero : ")))
 
 # res=n1+n2
 
@@ -41,3 +41,21 @@
 
 # print("Multiplicacion de numeros: ", x)
 
+
+def area_perimetro_rectangulo (base,altura):
+    """
+    Esta funcion toma dos parametros y devuelve su perimetro y su area 
+    """
+    perimetro=0
+    area=0
+    perimetro=base*2 + altura *2
+    area= base*altura
+    return perimetro , area
+
+x=int(input("Base: "))
+y=int(input("Altura: "))
+
+a, b = area_perimetro_rectangulo(x,y)
+
+print ( "El area de ",x,"y",y,"es igual a ",b)
+print ( "El perimetro de ",x,"y",y,"es igual a ",a)
